494ed07c4d5ecc717ebe0e3c5da2b25c1e3377a6
foxfire201 <138830027+foxfire201@users.noreply.github.com>
2024-12-13 14:20:20 +0000

    Update master.lua

--- master.lua
+++ master.lua
@@ -3,6 +3,7 @@
       Ability Wars -> 8260276694
       Word Bomb -> 2653064683
       Risky Haul -> 18421734073
+      APG -> 6405807112
       More Game Coming Soon -> 000000001
     }
 ]]
@@ -12,6 +13,8 @@ elseif game.PlaceId == 2653064683 then
     loadstring(game:HttpGet'https://foxfire201.github.io/scripts/WordBomb')()
 elseif game.PlaceId == 18421734073 then
     loadstring(game:HttpGet'https://foxfire201.github.io/scripts/RiskyHauw')()
+elseif game.PlaceId == 6405807112 then
+    loadstring(game:HttpGet'https://foxfire201.github.io/scripts/APG')()
 end
 
 return(function(a,b)
